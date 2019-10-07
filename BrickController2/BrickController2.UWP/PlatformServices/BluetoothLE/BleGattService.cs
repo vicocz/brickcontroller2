7865cd753b098cf6919c82fac6bdb2c3f489276d
vicocz <vico@centrum.cz>
2019-10-07 19:57:57 +0200

    Dispose all GATT services when device disconnects

--- BrickController2/BrickController2.UWP/PlatformServices/BluetoothLE/BleGattService.cs
+++ BrickController2/BrickController2.UWP/PlatformServices/BluetoothLE/BleGattService.cs
@@ -5,7 +5,7 @@ using Windows.Devices.Bluetooth.GenericAttributeProfile;
 
 namespace BrickController2.Windows.PlatformServices.BluetoothLE
 {
-    internal class BleGattService : IGattService
+    internal class BleGattService : IGattService, IDisposable
     {
         public BleGattService(GattDeviceService bluetoothGattService, IEnumerable<BleGattCharacteristic> characteristics)
         {
@@ -16,5 +16,22 @@ namespace BrickController2.Windows.PlatformServices.BluetoothLE
         public GattDeviceService BluetoothGattService { get; }
         public Guid Uuid => BluetoothGattService.Uuid;
         public IEnumerable<IGattCharacteristic> Characteristics { get; }
+
+        private bool disposed;
+
+        public void Dispose()
+        {
+            try
+            {
+                if (!disposed)
+                {
+                    disposed = true;
+                    BluetoothGattService.Dispose();
+                }
+            }
+            catch (ObjectDisposedException)
+            { 
+            }
+        }
     }
 }

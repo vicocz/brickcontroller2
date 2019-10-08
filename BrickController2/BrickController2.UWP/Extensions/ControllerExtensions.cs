35372cbee4246b97c07f351da073271d46a7d527
vicocz <vico@centrum.cz>
2019-10-08 18:53:40 +0200

    app prototype for UWP platform - basic functionality

---
+++ BrickController2/BrickController2.UWP/Extensions/ControllerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.Gaming.Input;
+
+namespace BrickController2.Windows.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static float ToControllerValue(this double value)
+        {
+            if (Math.Abs(value) < 0.05)
+            {
+                return 0.0F;
+            }
+            if (value > 0.95)
+            {
+                return 1.0F;
+            }
+            if (value < -0.95)
+            {
+                return -1.0F;
+            }
+            return (float)value;
+        }
+
+        public static string GetDeviceId(this Gamepad gamepad)
+        {
+            // kinda hack
+            return gamepad.User.NonRoamableId;
+        }
+    }
+}
---
+++ BrickController2/BrickController2.UWP/PlatformServices/DI/PlatformServicesModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using BrickController2.Windows.PlatformServices.BluetoothLE;
+using BrickController2.PlatformServices.BluetoothLE;
+using BrickController2.PlatformServices.GameController;
+using BrickController2.PlatformServices.Infrared;
+using BrickController2.PlatformServices.Localization;
+using BrickController2.PlatformServices.Preferences;
+using BrickController2.PlatformServices.Versioning;
+using BrickController2.Windows.PlatformServices.Infrared;
+using BrickController2.Windows.PlatformServices.Versioning;
+using BrickController2.Windows.PlatformServices.Localization;
+using BrickController2.Windows.PlatformServices.GameController;
+
+namespace BrickController2.Windows.PlatformServices.DI
+{
+    public class PlatformServicesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<InfraredService>().As<IInfraredService>().SingleInstance();
+            builder.RegisterType<GameControllerService>().AsSelf().As<IGameControllerService>().SingleInstance();
+            builder.RegisterType<VersionService>().As<IVersionService>().SingleInstance();
+            builder.RegisterType<BleService>().As<IBluetoothLEService>().SingleInstance();
+            builder.RegisterType<LocalizationService>().As<ILocalizationService>().SingleInstance();
+            builder.RegisterType<Preferences.Preferences>().As<IPreferences>().SingleInstance();
+        }
+    }
+}

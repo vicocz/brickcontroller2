72340b66bafcef7f9d4527fa1dc5169b5e728d8f
vicocz <vico@centrum.cz>
2019-11-17 12:17:26 +0100

    Allow to create copy of a controller profile under new name

--- BrickController2/BrickController2/Resources/TranslationResources.Designer.cs
+++ BrickController2/BrickController2/Resources/TranslationResources.Designer.cs
@@ -239,6 +239,23 @@ namespace BrickController2.Resources {
             }
         }
         
+        internal static string Copy {
+            get {
+                return ResourceManager.GetString("Copy", resourceCulture);
+            }
+        }
+        
+        internal static string CopyControllerProfile {
+            get {
+                return ResourceManager.GetString("CopyControllerProfile", resourceCulture);
+            }
+        }
+        
+        internal static string Copying {
+            get {
+                return ResourceManager.GetString("Copying", resourceCulture);
+            }
+        }            
         internal static string Create {
             get {
                 return ResourceManager.GetString("Create", resourceCulture);
@@ -478,6 +495,12 @@ namespace BrickController2.Resources {
                 return ResourceManager.GetString("ProfileName", resourceCulture);
             }
         }
+        
+        internal static string ProfileNameAlreadyExists {
+            get {
+                return ResourceManager.GetString("ProfileNameAlreadyExists", resourceCulture);
+            }
+        }        
         
         internal static string ProfileNameCanNotBeEmpty {
             get {

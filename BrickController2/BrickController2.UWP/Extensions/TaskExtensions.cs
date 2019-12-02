7222dc022b6cdcb5385e2b81410a4e1fa4eafc8f
vicocz <vico@centrum.cz>
2019-12-02 20:26:28 +0100

    app prototype for UWP platform - basic functionality

---
+++ BrickController2/BrickController2.UWP/Extensions/TaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BrickController2.Helpers;
+using System.Threading.Tasks;
+
+namespace BrickController2.Windows.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            task.ContinueWith(
+                        t => { Log.Error("The task has failed.", t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
---
+++ BrickController2/BrickController2.UWP/PlatformServices/BluetoothLE/BleGattService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BrickController2.PlatformServices.BluetoothLE;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+
+namespace BrickController2.Windows.PlatformServices.BluetoothLE
+{
+    internal class BleGattService : IGattService
+    {
+        public BleGattService(GattDeviceService bluetoothGattService, IEnumerable<BleGattCharacteristic> characteristics)
+        {
+            BluetoothGattService = bluetoothGattService;
+            Characteristics = characteristics;
+        }
+        
+        public GattDeviceService BluetoothGattService { get; }
+        public Guid Uuid => BluetoothGattService.Uuid;
+        public IEnumerable<IGattCharacteristic> Characteristics { get; }
+    }
+}

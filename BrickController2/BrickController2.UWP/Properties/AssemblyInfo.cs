c7a877f9a4948412f28720c54b9c224ab99ec9f9
vicocz <vico@centrum.cz>
2019-12-02 20:26:28 +0100

    app prototype for UWP platform - basic functionality

---
+++ BrickController2/BrickController2.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BrickController2")]
+[assembly: AssemblyDescription("Cross platform mobile application for controlling Lego creations using a bluetooth gamepad.")]
+[assembly: AssemblyProduct("BrickController2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]

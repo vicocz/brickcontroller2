8a4aac4ece5f36be4aaac22e740ce3b91ac81a7a
Vit Nemecky <vico@centrum.cz>
2021-10-12 21:49:19 +0200

    Workaround to hide navigation page Title as it's already present in TitleView

---
+++ BrickController2/BrickController2.UWP/UI/CustomRenderers/NavPageOverrideRenderer.cs
@@ -0,0 +1,51 @@
+﻿using BrickController2.Windows.UI.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(NavPageOverrideRenderer))]
+namespace BrickController2.Windows.UI.CustomRenderers
+{
+    public class NavPageOverrideRenderer : NavigationPageRenderer
+    {
+        public NavPageOverrideRenderer()
+        {
+        }
+
+        private void WorkaroundHideTitle(Page page)
+        {
+            page.Title = string.Empty;
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            if (e.NewElement is NavigationPage page)
+            {
+                WorkaroundHideTitle(page.CurrentPage);
+            }
+
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnPopRequested(object sender, NavigationRequestedEventArgs e)
+        {
+            WorkaroundHideTitle(e.Page);
+
+            base.OnPopRequested(sender, e);
+        }
+
+        protected override void OnPopToRootRequested(object sender, NavigationRequestedEventArgs e)
+        {
+            WorkaroundHideTitle(e.Page);
+
+            base.OnPopToRootRequested(sender, e);
+        }
+
+        protected override void OnPushRequested(object sender, NavigationRequestedEventArgs e)
+        {
+            WorkaroundHideTitle(e.Page);
+
+            base.OnPushRequested(sender, e);
+        }
+    }
+}

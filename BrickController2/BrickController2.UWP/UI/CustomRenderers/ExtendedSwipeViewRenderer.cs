29c80928a1b061f9ca5fc2ef5dc3f01768b14560
Vit Nemecky <vico@centrum.cz>
2021-11-20 10:39:36 +0100

    Workaround missing swipe support via mouse - call the first item's command on right mouse click

---
+++ BrickController2/BrickController2.UWP/UI/CustomRenderers/ExtendedSwipeViewRenderer.cs
@@ -0,0 +1,33 @@
+﻿using BrickController2.Windows.UI.CustomRenderers;
+using System.Linq;
+using Windows.UI.Xaml.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(SwipeView), typeof(ExtendedSwipeViewRenderer))]
+namespace BrickController2.Windows.UI.CustomRenderers
+{
+    public class ExtendedSwipeViewRenderer : SwipeViewRenderer
+    {
+        public ExtendedSwipeViewRenderer() : base()
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SwipeView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.RightTapped += Control_RightTapped;
+            }
+        }
+
+        private void Control_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            // invoke command of the first left item to suppport deletion (workaround for Windouws without touch controls)
+            var item = Element.LeftItems?.First();
+            item?.Command?.Execute(item?.CommandParameter);
+        }
+    }
+}

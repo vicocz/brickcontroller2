eef64cb717da7c76ed66b89cf3bac9d21ad57281
Vit Nemecky <vico@centrum.cz>
2021-11-27 01:25:17 +0100

    Handle HatX / HatY as axis

--- BrickController2/BrickController2.UWP/PlatformServices/GameController/GamepadMapping.cs
+++ BrickController2/BrickController2.UWP/PlatformServices/GameController/GamepadMapping.cs
@@ -19,28 +19,34 @@ namespace BrickController2.Windows.PlatformServices.GameController
         /// <summary>
         /// Set of virtual keys related to gamepad buttons
         /// </summary>
-        private static readonly Dictionary<VirtualKey, (string, float)> GamePadButtonMapping = new Dictionary<VirtualKey, (string, float)>()
+        private static readonly Dictionary<VirtualKey, string> GamePadButtonMapping = new Dictionary<VirtualKey, string>()
         {
-            { VirtualKey.GamepadA, ("ButtonA", Positive) },
-            { VirtualKey.GamepadB, ("ButtonB", Positive) },
-            { VirtualKey.GamepadX, ("ButtonX", Positive) },
-            { VirtualKey.GamepadY, ("ButtonY", Positive) },
+            { VirtualKey.GamepadA, "ButtonA" },
+            { VirtualKey.GamepadB, "ButtonB" },
+            { VirtualKey.GamepadX, "ButtonX" },
+            { VirtualKey.GamepadY, "ButtonY" },
 
-            { VirtualKey.GamepadLeftShoulder, ("ButtonL1", Positive) },
-            { VirtualKey.GamepadRightShoulder, ("ButtonR1", Positive) },
-            { VirtualKey.GamepadLeftTrigger, ("ButtonL2", Positive) },
-            { VirtualKey.GamepadRightTrigger, ("ButtonR2", Positive) },
+            { VirtualKey.GamepadLeftShoulder, "ButtonL1" },
+            { VirtualKey.GamepadRightShoulder, "ButtonR1" },
+            { VirtualKey.GamepadLeftTrigger, "ButtonL2" },
+            { VirtualKey.GamepadRightTrigger, "ButtonR2" },
 
+            { VirtualKey.GamepadMenu, "ButtonStart" },
+            { VirtualKey.GamepadView, "ButtonSelect" },
+
+            { VirtualKey.GamepadLeftThumbstickButton, "ButtonThumbl" },
+            { VirtualKey.GamepadRightThumbstickButton, "ButtonThumbr" },
+        };
+
+        /// <summary>
+        /// Set of virtual keys related to gamepad buttons handled as AXIS
+        /// </summary>
+        private static readonly Dictionary<VirtualKey, (string, float)> GamePadAxisMapping = new Dictionary<VirtualKey, (string, float)>()
+        {
             { VirtualKey.GamepadDPadUp, ("HatY", Negative) },
             { VirtualKey.GamepadDPadDown, ("HatY", Positive) },
             { VirtualKey.GamepadDPadLeft, ("HatX", Negative) },
             { VirtualKey.GamepadDPadRight, ("HatX", Positive) },
-
-            { VirtualKey.GamepadMenu, ("ButtonStart", Positive) },
-            { VirtualKey.GamepadView, ("ButtonSelect", Positive) },
-
-            { VirtualKey.GamepadLeftThumbstickButton, ("ButtonThumbl", Positive) },
-            { VirtualKey.GamepadRightThumbstickButton, ("ButtonThumbr", Positive) },
         };
 
         /// <summary>
@@ -48,18 +54,29 @@ namespace BrickController2.Windows.PlatformServices.GameController
         /// </summary>
         private static readonly HashSet<string> GamePadInvertedAxis = new HashSet<string> { YAxis, RzAxis };
 
-        public static bool IsGamepadButton(VirtualKey virtualKey, out string buttonCode, out float buttonValue)
+        public static bool IsGamepadButton(VirtualKey virtualKey, out string buttonCode)
         {
-            if (GamePadButtonMapping.TryGetValue(virtualKey, out (string, float) button))
+            if (GamePadButtonMapping.TryGetValue(virtualKey, out buttonCode))
             {
-                buttonCode = button.Item1;
-                buttonValue = button.Item2;
-
                 return true;
             }
 
             buttonCode = default;
-            buttonValue = default;
+            return false;
+        }
+
+        public static bool IsGamepadAxis(VirtualKey virtualKey, out string axisCode, out float axisValue)
+        {
+            if (GamePadAxisMapping.TryGetValue(virtualKey, out (string, float) button))
+            {
+                axisCode = button.Item1;
+                axisValue = button.Item2;
+
+                return true;
+            }
+
+            axisCode = default;
+            axisValue = default;
 
             return false;
         }

30a4a7ac65e154cbbecc8a869893396609b9b62a
Vit Nemecky <vico@centrum.cz>
2021-12-06 20:03:24 +0100

    Basic support for Buwizz3 servro and stepper + UI adjustments

--- BrickController2/BrickController2/Helpers/ByteArrayExtensions.cs
+++ BrickController2/BrickController2/Helpers/ByteArrayExtensions.cs
@@ -19,5 +19,27 @@ namespace BrickController2.Helpers
 
             return null;
         }
+
+        public static void SetInt32(this byte[] data, int offset, int value)
+        {
+            data[offset + 0] = (byte)(value & 0xff);
+            data[offset + 1] = (byte)((value >> 8) & 0xff);
+            data[offset + 2] = (byte)((value >> 16) & 0xff);
+            data[offset + 3] = (byte)((value >> 24) & 0xff);
+        }
+
+        public static short GetInt16(this byte[] data, int offset)
+        {
+            return (short)(data[offset] |
+                (data[offset + 1] << 8));
+        }
+
+        public static int GetInt32(this byte[] data, int offset)
+        {
+            return data[offset] |
+                (data[offset + 1] << 8) |
+                (data[offset + 2] << 16) |
+                (data[offset + 3] << 24);
+        }
     }
 }

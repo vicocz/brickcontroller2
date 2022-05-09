b57a9252f871739ae2d8c9e24c31bce950f935b2
Vit Nemecky <vico@centrum.cz>
2022-05-09 22:07:39 +0200

    Add UWP implementation of IBluetoothPermission

---
+++ BrickController2/BrickController2.UWP/PlatformServices/Permission/BluetoothPermission.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using static Xamarin.Essentials.Permissions;
+using BrickController2.PlatformServices.Permission;
+using System;
+
+namespace BrickController2.Windows.PlatformServices.Permission
+{
+    public class BluetoothPermission : BasePlatformPermission, IBluetoothPermission
+    {
+        protected override Func<IEnumerable<string>> RequiredDeclarations => () => new[] { "bluetooth" };
+    }
+}

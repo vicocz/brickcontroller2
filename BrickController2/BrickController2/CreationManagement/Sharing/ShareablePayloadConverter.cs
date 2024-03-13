5251583a2b5e93ef9bd9ff93bed4ca8992c2ec0e
Vit Nemecky <vit.nemecky@centrum.cz>
2024-03-13 23:08:27 +0100

    Export / import a creation via QR code (#33)
    * Export / import a creation via QR code
    * button icon fix + proper CanExecute enablement
    * renaming pages/vm
    
    ---------
    
    Co-authored-by: Vit Nemecky <vico@centrum.cz>

---
+++ BrickController2/BrickController2/CreationManagement/Sharing/ShareablePayloadConverter.cs
@@ -0,0 +1,106 @@
+﻿using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BrickController2.CreationManagement.Sharing;
+
+internal class ShareablePayloadConverter<TModel> : JsonConverter<ShareablePayload<TModel>>
+    where TModel : class, IShareable
+{
+    // reasonable value to optimize both json text readibility and pixel size of rendered QR
+    private const int MaxSize = 1600;
+
+    public override ShareablePayload<TModel> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+            throw new JsonException();
+
+        TModel payload = default;
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                return new(payload);
+
+            if (reader.TokenType != JsonTokenType.PropertyName)
+                throw new JsonException();
+
+            var propertyName = reader.GetString();
+            reader.Read();
+            switch (propertyName)
+            {
+                case ShareablePayload<TModel>.ContentTypeProperty:
+                    // validate expected content type
+                    var contentType = reader.GetString();
+                    if (contentType != TModel.Type)
+                        throw new JsonException($"Unsuppported conent type: {contentType}.");
+                    break;
+                case ShareablePayload<TModel>.PayloadProperty:
+                    // load payload
+                    payload = DeserializePayload(ref reader, options);
+                    break;
+
+                default:
+                    // unknown property
+                    throw new JsonException($"Unexpected property: {propertyName}.");
+            }
+        }
+
+        throw new JsonException();
+    }
+
+    private static TModel DeserializePayload(ref Utf8JsonReader reader, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.StartObject:
+                // directly deserialize payload
+                return JsonSerializer.Deserialize<TModel>(ref reader, options);
+
+            case JsonTokenType.String:
+                // unzip Base64 payload string
+                {
+                    using var output = new MemoryStream();
+                    using var input = new MemoryStream(reader.GetBytesFromBase64());
+                    using var unzip = new GZipStream(input, CompressionMode.Decompress);
+                    unzip.CopyTo(output);
+                    output.Position = 0;
+
+                    return JsonSerializer.Deserialize<TModel>(output, options);
+                }
+
+            default:
+                throw new JsonException($"Unexpected token type: {reader.TokenType}.");
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, ShareablePayload<TModel> model, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        // write content type of the model
+        writer.WriteString(ShareablePayload<TModel>.ContentTypeProperty, TModel.Type);
+        // write payload based on size autodetection
+        var payload = JsonSerializer.Serialize(model.Payload, options);
+
+        writer.WritePropertyName(ShareablePayload<TModel>.PayloadProperty);
+
+        // autodetect final format based on source payload size
+        if (payload.Length < MaxSize)
+        {
+            writer.WriteRawValue(payload);
+        }
+        else
+        {
+            using var output = new MemoryStream();
+            using var zip = new GZipStream(output, CompressionMode.Compress);
+            zip.Write(Encoding.UTF8.GetBytes(payload));
+            zip.Flush();
+
+            output.TryGetBuffer(out var buffer);
+            writer.WriteBase64StringValue(buffer.AsSpan());
+        }
+
+        writer.WriteEndObject();
+    }
+}
--- BrickController2/BrickController2/CreationManagement/Sharing/SharingManager.cs
+++ BrickController2/BrickController2/CreationManagement/Sharing/SharingManager.cs
@@ -11,24 +11,31 @@ public class SharingManager<TModel> : ISharingManager<TModel> where TModel : cla
         {
             DefaultValueHandling = DefaultValueHandling.Ignore
         };
+        // compact options using auto gzip converter
+        CompactJsonOptions = new JsonSerializerOptions(JsonOptions);
+        CompactJsonOptions.Converters.Add(new ShareablePayloadConverter<TModel>());
     }
 
     internal JsonSerializerSettings JsonOptions { get; }
+    internal JsonSerializerSettings CompactJsonOptions { get; }
+
+    /// <inheritdoc/>
+    public Task<string> ShareAsync(TModel model) => ShareAsync(model, CompactJsonOptions);
 
     /// <summary>
     /// Export the specified <paramref name="item"/> as serialized JSON model
     /// </summary>
-    internal Task<string> ShareAsync(TModel model)
+    internal static Task<string> ShareAsync(TModel model, JsonSerializerOptions options)
     {
         var payload = new ShareablePayload<TModel>(model);
-        var json = JsonConvert.SerializeObject(payload, JsonOptions);
+        var json = JsonSerializer.Serialize(payload, options);
         return Task.FromResult(json);
     }
 
     /// <inheritdoc/>
     public async Task ShareToClipboardAsync(TModel model)
     {
-        var json = await ShareAsync(model);
+        var json = await ShareAsync(model, JsonOptions);
         await MainThread.InvokeOnMainThreadAsync(() =>
         {
             Clipboard.SetTextAsync(json);
@@ -39,12 +46,15 @@ public class SharingManager<TModel> : ISharingManager<TModel> where TModel : cla
     public async Task<TModel> ImportFromClipboardAsync()
     {
         var json = await MainThread.InvokeOnMainThreadAsync(Clipboard.GetTextAsync);
-        return Import(json);
+        return Import(json, JsonOptions);
     }
 
-    internal TModel Import(string json)
+    /// <inheritdoc/>
+    public TModel Import(string json) => Import(json, CompactJsonOptions);
+
+    internal static TModel Import(string json, JsonSerializerOptions options)
     {
-        var model = JsonConvert.DeserializeObject<ShareablePayload<TModel>>(json, JsonOptions);
+        var model = JsonConvert.DeserializeObject<ShareablePayload<TModel>>(json, options);
 
         if (model?.Payload is null)
             throw new InvalidOperationException("Invalid json data.");

c00b78b9172efdc2d05fda59d4acb35292356732
Vit Nemecky <vico@centrum.cz>
2024-11-17 18:10:12 +0100

    work save

--- BrickController2/BrickController2/DeviceManagement/Device.cs
+++ BrickController2/BrickController2/DeviceManagement/Device.cs
@@ -149,7 +149,7 @@ namespace BrickController2.DeviceManagement
             _settings[settingName] = new DeviceSetting
             {
                 Name = settingName,
-                Value = foundSetting?.Value ?? defaultValue
+                Value = foundSetting?.Value is null ? defaultValue : foundSetting.GetValue(defaultValue)
             };
         }
 
--- BrickController2/BrickController2/DeviceManagement/DeviceSetting.cs
+++ BrickController2/BrickController2/DeviceManagement/DeviceSetting.cs
@@ -16,4 +16,22 @@ public record DeviceSetting
     /// <summary>Current setting value</summary>
     [JsonProperty]
     public object Value { get; set; } = default!;
+
+    public TValue GetValue<TValue>(TValue defaultValue)
+    {
+        // special handling of enums
+        if (typeof(TValue).IsEnum)
+        {
+            var safeValue = Convert.ChangeType(Value, Enum.GetUnderlyingType(typeof(TValue)));
+            if (Enum.IsDefined(typeof(TValue), safeValue))
+            {
+                return (TValue)Enum.ToObject(typeof(TValue), safeValue);
+            }
+        }
+        else
+        {
+            return (TValue)Value;
+        }
+        return defaultValue;
+    }
 }

caec84aa13c9d31aee595596ed0f3944a2028a29
Vit Nemecky <vico@centrum.cz>
2024-11-25 20:11:06 +0100

    Fix initial value of output level

--- BrickController2/BrickController2/DeviceManagement/BuWizz2Device.cs
+++ BrickController2/BrickController2/DeviceManagement/BuWizz2Device.cs
@@ -51,6 +51,8 @@ namespace BrickController2.DeviceManagement
             // apply values (if any) or default
             SetSettingValue(SwapChannelsSettingName, settings, swapChannels);
             SetSettingValue(DefaultOutputLevelName, settings, DefaultLevel);
+            // update ooutput value again to apply settings
+            _outputLevel = DefaultOutputLevel;
         }
 
         public override DeviceType DeviceType => DeviceType.BuWizz2;
--- BrickController2/BrickController2/DeviceManagement/BuwizzDevice.cs
+++ BrickController2/BrickController2/DeviceManagement/BuwizzDevice.cs
@@ -33,6 +33,8 @@ namespace BrickController2.DeviceManagement
         {
             // apply values (if any) or default
             SetSettingValue(DefaultOutputLevelName, settings, DefaultLevel);
+            // update ooutput value again to apply settings
+            _outputLevel = DefaultOutputLevel;
         }
 
         public override DeviceType DeviceType => DeviceType.BuWizz;

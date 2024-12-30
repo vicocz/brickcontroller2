db089a228609c879114c78097c53f6f0693ea7aa
Vit Nemecky <vico@centrum.cz>
2024-12-30 22:10:24 +0100

    Disable LUT for motor commands

--- BrickController2/BrickController2/Protocols/BuWizz3Protocol.cs
+++ BrickController2/BrickController2/Protocols/BuWizz3Protocol.cs
@@ -4,6 +4,11 @@ internal static class BuWizz3Protocol
 {
     public const double CurrentLimitStep = 35;
 
+    public const byte CMD_SET_CURRENT_LIMITS = 0x38;
+
+    public const byte MOTOR_BREAKS_NONE = 0x00;
+    public const byte MOTOR_LUT_DISABLE_ALL = 0x3f;
+
     public static byte[] ActivteShelfMode() => [ 0xA1 ];
 
     public static byte[] SetCurrentLimits(double v0, double v1, double v2, double v3, double v4, double v5)
@@ -14,7 +19,7 @@ internal static class BuWizz3Protocol
         byte l3 = ConvertToRaw(v3);
         byte lA = ConvertToRaw(v4);
         byte lB = ConvertToRaw(v5);
-        return [0x38, l0, l1, l2, l3, lA, lB];
+        return [CMD_SET_CURRENT_LIMITS, l0, l1, l2, l3, lA, lB];
 
         static byte ConvertToRaw(double currentLimit)
             => (byte)(currentLimit / CurrentLimitStep);

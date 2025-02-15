4203caaca26e9c8e06bba41465deaab449972e7d
Vit Nemecky <vico@centrum.cz>
2025-02-15 19:43:12 +0100

    Fix build error due to my fork diffs

--- BrickController2/BrickController2/DeviceManagement/BluetoothDeviceManager.cs
+++ BrickController2/BrickController2/DeviceManagement/BluetoothDeviceManager.cs
@@ -88,7 +88,7 @@ namespace BrickController2.DeviceManagement
                     }
                     break;
                 case "05-45": // BuWizz2 has new ID since firmware 1.2.30
-                    if (advertismentData.TryGetValue(ADTYPE_LOCAL_NAME_COMPLETE, out byte[]? buwizzName))
+                    if (advertismentData.TryGetValue(0x09, out byte[]? buwizzName))
                     {
                         var completeLocalNameString = BitConverter.ToString(buwizzName).ToLower();
                         if (completeLocalNameString == "42-75-57-69-7a-7a-32") // BuWizz2

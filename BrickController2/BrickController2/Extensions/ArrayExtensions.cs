7e2e35c9d2da09298707a7cdd42fa48ba76e488c
Vit Nemecky <vico@centrum.cz>
2025-04-23 22:35:26 +0200

    nitpicks

--- BrickController2/BrickController2/Extensions/ArrayExtensions.cs
+++ BrickController2/BrickController2/Extensions/ArrayExtensions.cs
@@ -5,6 +5,10 @@ namespace BrickController2.Extensions;
 
 public static class ArrayExtensions
 {
+    /// <summary>
+    /// This method is a concise, efficient, and null-safe way to compare two arrays of <typeparamref name="TItem"/> for equality,
+    /// leveraging modern C# features like spans.
+    /// </summary>
     public static bool SequenceEqual<TItem>(this TItem[]? x, TItem[]? y)
     {
         if (x != null && y != null)

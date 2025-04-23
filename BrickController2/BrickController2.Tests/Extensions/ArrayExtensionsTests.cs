f04c04c1c584f3bbf3136ef1689f7ee8da7ddd97
Vit Nemecky <vico@centrum.cz>
2025-04-23 22:17:55 +0200

    Run unit tests for BrickController core project

---
+++ BrickController2/BrickController2.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using BrickController2.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace BrickController2.Tests.Extensions;
+
+public class ArrayExtensionsTests
+{
+    [Fact]
+    public void SequenceEqual_BothArraysNull_ReturnsTrue()
+    {
+        ArrayExtensions.SequenceEqual<int>(null, null).Should().BeTrue();
+    }
+
+    [Fact]
+    public void SequenceEqual_OneArrayNull_ReturnsFalse()
+    {
+        ArrayExtensions.SequenceEqual<string>(null, []).Should().BeFalse();
+        ArrayExtensions.SequenceEqual(["a"], null).Should().BeFalse();
+    }
+
+    [Fact]
+    public void SequenceEqual_ArraysWithSameElements_ReturnsTrue()
+    {
+        // Arrange
+        int[] x = [1, 2, 3];
+        int[] y = [1, 2, 3];
+
+        // Act & Assert
+        x.SequenceEqual(y).Should().BeTrue();
+    }
+
+    [Fact]
+    public void SequenceEqual_ArraysWithDifferentElements_ReturnsFalse()
+    {
+        // Arrange
+        int[] x = [1, 2, 3];
+        int[] y = [4, 5, 6];
+
+        // Act & Assert
+        x.SequenceEqual(y).Should().BeFalse();
+    }
+
+    [Fact]
+    public void SequenceEqual_ArraysWithDifferentLengths_ReturnsFalse()
+    {
+        // Arrange
+        short[] x = [1, 2, 3];
+        short[] y = { 1, 2 };
+
+        // Act & Assert
+        x.SequenceEqual(y).Should().BeFalse();
+    }
+}
---
+++ BrickController2/BrickController2/Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace BrickController2.Extensions;
+
+public static class ArrayExtensions
+{
+    public static bool SequenceEqual<TItem>(this TItem[]? x, TItem[]? y)
+    {
+        if (x != null && y != null)
+        {
+            return x.AsSpan().SequenceEqual(y);
+        }
+
+        return x == null && y == null;
+    }
+}

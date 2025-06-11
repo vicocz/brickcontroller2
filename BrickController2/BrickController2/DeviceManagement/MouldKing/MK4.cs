aa677ecabc0769c2ed4afde7ac4ac3d5e2093c7d
J0EK3R <J0EK3R3.3@WIN11>
2025-06-11 06:09:27 +0200

    JK: revised MKBaseNibble and MK4.0

--- BrickController2/BrickController2/DeviceManagement/MouldKing/MK4.cs
+++ BrickController2/BrickController2/DeviceManagement/MouldKing/MK4.cs
@@ -14,17 +14,22 @@ namespace BrickController2.DeviceManagement
         public const string Device3 = "Device3";
 
         /// <summary>
-        /// offset to position of first channel in base telegram
+        /// offset to position of first analog channel in base telegram
         /// </summary>
         private const int ChannelStartOffset = 3;
 
         /// <summary>
-        /// Telegram wich is sent to connect to MK4.0
+        /// Telegram to connect to the MK4.0 device(s)
+        /// This telegram is sent on init and on reconnect conditions matching
         /// </summary>
         private static readonly byte[] Telegram_Connect = new byte[] { 0xAD, 0x7B, 0xA7, 0x80, 0x80, 0x80, 0x4F, 0x52 };
 
         /// <summary>
-        /// Base Telegram for MK4
+        /// Base Telegram for MK4.0
+        /// -> this telegram handles all three MK4.0 devices
+        /// * channels 0..3 for Device1 start at offset 3 and are analog channels
+        /// * channels 0..3 for Device2 start at offset 5 and are analog channels
+        /// * channels 0..3 for Device3 start at offset 7 and are analog channels
         /// </summary>
         private static readonly byte[] Telegram_Base = new byte[] { 0x7D, 0x7B, 0xA7, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x82 };
 
@@ -45,6 +50,7 @@ namespace BrickController2.DeviceManagement
 
         /// <summary>
         /// number of bytes containing channel values in base telegram
+        /// -> channel 0..3 for all three devices
         /// </summary>
         protected override int BaseTelegram_ChannelBytesCount => 6;
 
@@ -54,12 +60,16 @@ namespace BrickController2.DeviceManagement
         protected override int BaseTelegram_ChannelStartOffset => 3;
 
         public MK4(string name, string address, byte[] deviceData, IDeviceRepository deviceRepository, IBluetoothLEService bleService, IMKPlatformService mkPlatformService)
-          : base(name, address, deviceData, deviceRepository, bleService, MK4.GetChannelStartOffset(address), MK4.Telegram_Connect, MK4.Telegram_Base, mkPlatformService)
+          : base(name, address, deviceData, deviceRepository, bleService, mkPlatformService, MK4.GetChannelStartOffset(address), MK4.Telegram_Connect, MK4.Telegram_Base)
         {
         }
 
         public override DeviceType DeviceType => DeviceType.MK4;
 
+        /// <summary>
+        /// Number of channels for one MK4.0 device
+        /// * channel 0..3 are analog 
+        /// </summary>
         public override int NumberOfChannels => 4;
 
         /// <summary>
--- BrickController2/BrickController2/DeviceManagement/MouldKing/MKBaseNible.cs
+++ BrickController2/BrickController2/DeviceManagement/MouldKing/MKBaseNible.cs
@@ -1,151 +1,285 @@
 ﻿using System;
 using BrickController2.PlatformServices.BluetoothLE;
 
-namespace BrickController2.DeviceManagement
+namespace BrickController2.DeviceManagement.MouldKing;
+
+/// <summary>
+/// MK baseclass for devices with a nibble per channel
+/// </summary>
+internal abstract class MKBaseNible : BluetoothAdvertisingDevice
 {
     /// <summary>
-    /// MK baseclass for devices with a nibble per channel
+    /// Channel types
     /// </summary>
-    internal abstract class MKBaseNible : BluetoothAdvertisingDevice
+    protected enum ChannelType
     {
         /// <summary>
-        /// platform specific MouldKing stuff
+        /// channel is analog
         /// </summary>
-        protected readonly IMKPlatformService _mkPlatformService;
+        Analog,
 
         /// <summary>
-        /// Telegram to connect to the device
-        /// This telegram is sent on init and on reconnect conditions matching
+        /// channel can be set to left, off or right
         /// </summary>
-        protected readonly byte[] _telegram_Connect;
+        Left_Off_Right
+    }
 
-        /// <summary>
-        /// base telegram
-        /// </summary>
-        protected readonly byte[] _telegram_Base;
+    /// <summary>
+    /// platform specific MouldKing stuff
+    /// </summary>
+    protected readonly IMKPlatformService _mkPlatformService;
 
-        /// <summary>
-        /// byte offset to first channel in _telegram_base
-        /// </summary>
-        protected readonly int _channelStartOffset;
+    /// <summary>
+    /// Telegram to connect to the device
+    /// This telegram is sent on init and on reconnect conditions matching
+    /// </summary>
+    protected readonly byte[] _telegram_Connect;
 
-        protected MKBaseNible(string name, string address, byte[] deviceData, IDeviceRepository deviceRepository, IBluetoothLEService bleService, int channelStartOffset, byte[] telegram_Connect, byte[] telegram_Base, IMKPlatformService mkPlatformService)
-            : base(name, address, deviceData, deviceRepository, bleService)
+    /// <summary>
+    /// base telegram
+    /// </summary>
+    protected readonly byte[] _telegram_Base;
+
+    /// <summary>
+    /// byte offset to first channel in _telegram_base
+    /// </summary>
+    protected readonly int _channelStartOffset;
+
+    /// <summary>
+    /// combined low and high nibble of zeroValue
+    /// </summary>
+    protected readonly byte _zeroValueByte;
+
+    /// <summary>
+    /// range for negative values
+    /// </summary>
+    protected readonly int _range_neg;
+
+    /// <summary>
+    /// range for positive values
+    /// </summary>
+    protected readonly int _range_pos;
+
+    protected MKBaseNible(string name, string address, byte[] deviceData, IDeviceRepository deviceRepository, IBluetoothLEService bleService, IMKPlatformService mkPlatformService, int channelStartOffset, byte[] telegram_Connect, byte[] telegram_Base)
+        : base(name, address, deviceData, deviceRepository, bleService)
+    {
+        _channelStartOffset = channelStartOffset;
+        _telegram_Connect = telegram_Connect;
+        _telegram_Base = telegram_Base;
+        _mkPlatformService = mkPlatformService;
+
+        int zeroOffset = ZeroValueNibble - 0x08;    // offset to default value of 0x08
+        _range_pos = 0x07 - zeroOffset;             // if zeroValue is > 0x08 then range_pos is smaller than 0x07
+        _range_neg = 0x07;
+
+        _zeroValueByte = (byte)((ZeroValueNibble << 4) + ZeroValueNibble); // combined low and high nibble of zeroValue
+    }
+
+    /// <summary>
+    /// No voltage
+    /// </summary>
+    public override string BatteryVoltageSign => string.Empty;
+
+    /// <summary>
+    /// byte offset to position of first channel in base telegram
+    /// </summary>
+    protected abstract int BaseTelegram_ChannelStartOffset { get; }
+
+    /// <summary>
+    /// zeroValue - default is 0x08, MK3.8 has 0x09
+    /// </summary>
+    protected virtual byte ZeroValueNibble => 0x08;
+
+    /// <summary>
+    /// number of bytes containing channel values in base telegram
+    /// </summary>
+    protected abstract int BaseTelegram_ChannelBytesCount { get; }
+
+    /// <summary>
+    /// Sets the output value for the specified channel.
+    /// </summary>
+    /// <remarks>This method adjusts the output value based on the channel type and applies the appropriate
+    /// encoding. If the output value changes, a notification is triggered to indicate the change. For channels where
+    /// the value is set to zero, additional checks are performed to determine if all channels are zero.</remarks>
+    /// <param name="channelNo">The channel number for which the output value is to be set. Must be a valid channel index.</param>
+    /// <param name="value">The output value to set. The value is interpreted based on the channel type: negative values, zero, and positive
+    /// values may have different effects depending on the channel configuration.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the channel type is unknown or unsupported.</exception>
+    public override void SetOutput(int channelNo, float value)
+    {
+        CheckChannel(channelNo);
+        value = CutOutputValue(value);
+
+        int channelOffset;
+        ChannelType channelType;
+        SelectChannel(channelNo, out channelOffset, out channelType);
+
+        byte originValue_byte = _telegram_Base[channelOffset];
+
+        byte setValue_byte;
+        bool zeroSet;
+
+        switch (channelType)
         {
-            _channelStartOffset = channelStartOffset;
-            _telegram_Connect = telegram_Connect;
-            _telegram_Base = telegram_Base;
-            _mkPlatformService = mkPlatformService;
-        }
+            case ChannelType.Analog:
+                // MK4: ZeroValueNibble = 0x08
+                // value <  0:  7 6 5 4 3 2 1                    range_neg: 0x07
+                // value == 0:                0 8                
+                // value >  0:                    9 A B C D E F  range_pos: 0x07
 
-        /// <summary>
-        /// No voltage
-        /// </summary>
-        public override string BatteryVoltageSign => string.Empty;
+                // MK3.8: ZeroValueNibble = 0x09                    
+                // value <  0:  7 6 5 4 3 2 1                    range_neg: 0x07
+                // value == 0:                0 9                
+                // value >  0:                    A B C D E F    range_pos: 0x06
 
-        /// <summary>
-        /// byte offset to position of first channel in base telegram
-        /// </summary>
-        protected abstract int BaseTelegram_ChannelStartOffset { get; }
-
-        /// <summary>
-        /// number of bytes containing channel values in base telegram
-        /// </summary>
-        protected abstract int BaseTelegram_ChannelBytesCount { get; }
-
-        public override void SetOutput(int channelNo, float value)
-        {
-            CheckChannel(channelNo);
-            value = CutOutputValue(value);
-
-            bool isOdd = (channelNo & 0x01) == 0x01;                    // is odd
-            int channelOffset = _channelStartOffset + (channelNo >> 1); // div 2
-            byte originValue_byte = _telegram_Base[channelOffset];
-            byte setValue_nibble;
-            byte setValue_byte;
-
-
-            if (value < 0)
-            {
-                float value_abs = Math.Min(0x07, -value * 0x07);
-                setValue_nibble = (byte)(0x0F & (byte)value_abs);
-            }
-            else if (value > 0)
-            {
-                float value_abs = Math.Min(0x07, value * 0x07);
-                setValue_nibble = (byte)(0x0F & (byte)(0x08 + value_abs));
-            }
-            else
-            {
-                setValue_nibble = 0x08;
-            }
-
-            if (isOdd)
-            {
-                setValue_byte = (byte)((originValue_byte & 0xF0) + setValue_nibble);
-            }
-            else
-            {
-                setValue_byte = (byte)((originValue_byte & 0x0F) + (setValue_nibble << 4));
-            }
-
-            lock (_outputLock)
-            {
-                // check for change
-                if (_telegram_Base[channelOffset] != setValue_byte)
+                byte setValue_nibble;
+                if (value < 0)
                 {
-                    _telegram_Base[channelOffset] = setValue_byte;
+                    float value_abs = Math.Min(0x07, -value * _range_neg);
+                    setValue_nibble = (byte)(0x0F & (byte)value_abs);
 
-                    // Zero was set -> check all channel's values
-                    if (setValue_nibble == 0x08)
+                    if (setValue_nibble == 0) // replace zero with ZeroValueNibble
                     {
-                        // notify data changed
-                        _bluetoothAdvertisingDeviceHandler.NotifyDataChanged(CheckAllChannelsZero());
+                        setValue_nibble = ZeroValueNibble;
+                        zeroSet = true;
                     }
                     else
                     {
-                        // notify data changed
-                        _bluetoothAdvertisingDeviceHandler.NotifyDataChanged(false);
+                        zeroSet = false;
                     }
                 }
-            }
-        }
-
-        /// <summary>
-        /// This method sets the device to initial state before advertising starts
-        /// </summary>
-        protected override void InitDevice()
-        {
-            // set all channels to zero
-            for (int index = 0; index < BaseTelegram_ChannelBytesCount; index++)
-            {
-                _telegram_Base[BaseTelegram_ChannelStartOffset + index] = 0x88;
-            }
-        }
-
-        protected bool TryGetTelegram(bool getConnectTelegram, out byte[] payload)
-        {
-            if (getConnectTelegram)
-            {
-                return _mkPlatformService.TryGetRfPayload(_telegram_Connect, out payload);
-            }
-            else
-            {
-                return _mkPlatformService.TryGetRfPayload(_telegram_Base, out payload);
-            }
-        }
-
-        private bool CheckAllChannelsZero()
-        {
-            for (int index = 0; index < BaseTelegram_ChannelBytesCount; index++)
-            {
-                if (_telegram_Base[BaseTelegram_ChannelStartOffset + index] != 0x88)
+                else if (value > 0)
                 {
-                    return false;
+                    float value_abs = Math.Min(0x0F, (value * _range_pos) + ZeroValueNibble);
+                    setValue_nibble = (byte)(0x0F & (byte)(value_abs));
+                    zeroSet = false;
+                }
+                else
+                {
+                    setValue_nibble = ZeroValueNibble;
+                    zeroSet = true;
+                }
+
+                bool isOdd = (channelNo & 0x01) == 0x01; // is odd
+                if (isOdd)
+                {
+                    setValue_byte = (byte)((originValue_byte & 0xF0) + setValue_nibble);
+                }
+                else
+                {
+                    setValue_byte = (byte)((originValue_byte & 0x0F) + (setValue_nibble << 4));
+                }
+
+                break;
+            case ChannelType.Left_Off_Right:
+
+                if (value < 0)
+                {
+                    setValue_byte = 1;
+                    zeroSet = false;
+                }
+                else if (value > 0)
+                {
+                    setValue_byte = 2;
+                    zeroSet = false;
+                }
+                else
+                {
+                    setValue_byte = 0;
+                    zeroSet = true;
+                }
+
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(channelType), $"Unknown channel type: {channelType}.");
+        }
+
+
+        lock (_outputLock)
+        {
+            // check for change
+            if (_telegram_Base[channelOffset] != setValue_byte)
+            {
+                _telegram_Base[channelOffset] = setValue_byte;
+
+                // Zero was set -> check all channel's values
+                if (zeroSet)
+                {
+                    // notify data changed
+                    _bluetoothAdvertisingDeviceHandler.NotifyDataChanged(CheckAllChannelsZero());
+                }
+                else
+                {
+                    // notify data changed
+                    _bluetoothAdvertisingDeviceHandler.NotifyDataChanged(false);
                 }
             }
-
-            return true;
         }
+    }
+
+    /// <summary>
+    /// This method sets the device to initial state before advertising starts
+    /// All channels are initialized with zeroValue.
+    /// </summary>
+    protected override void InitDevice()
+    {
+        // set all channels to zero
+        for (int index = 0; index < BaseTelegram_ChannelBytesCount; index++)
+        {
+            _telegram_Base[BaseTelegram_ChannelStartOffset + index] = _zeroValueByte;
+        }
+    }
+
+    /// <summary>
+    /// Selects a channel based on the specified channel number and provides its offset and type.
+    /// </summary>
+    /// <remarks>The method calculates the channel offset based on the provided channel number and assigns a
+    /// default channel type. Override this method in a derived class to customize channel selection behavior.</remarks>
+    /// <param name="channelNo">The number of the channel to select. Must be a non-negative integer.</param>
+    /// <param name="channelOffset">When this method returns, contains the calculated offset for the selected channel.</param>
+    /// <param name="channelType">When this method returns, contains the type of the selected channel.</param>
+    protected virtual void SelectChannel(int channelNo, out int channelOffset, out ChannelType channelType)
+    {
+        channelOffset = _channelStartOffset + (channelNo >> 1); // div 2
+        channelType = ChannelType.Analog;
+    }
+
+    /// <summary>
+    /// Attempts to retrieve the RF payload for the specified telegram type.
+    /// </summary>
+    /// <remarks>This method delegates the retrieval of the RF payload to the underlying platform
+    /// service.</remarks>
+    /// <param name="getConnectTelegram">A boolean value indicating the type of telegram to retrieve.  <see langword="true"/> to retrieve the connect
+    /// telegram; <see langword="false"/> to retrieve the base telegram.</param>
+    /// <param name="payload">When this method returns, contains the RF payload as a byte array if the operation succeeds; otherwise, <see
+    /// langword="null"/>.</param>
+    /// <returns><see langword="true"/> if the RF payload was successfully retrieved; otherwise, <see langword="false"/>.</returns>
+    protected bool TryGetTelegram(bool getConnectTelegram, out byte[] payload)
+    {
+        if (getConnectTelegram)
+        {
+            return _mkPlatformService.TryGetRfPayload(_telegram_Connect, out payload);
+        }
+        else
+        {
+            return _mkPlatformService.TryGetRfPayload(_telegram_Base, out payload);
+        }
+    }
+
+    /// <summary>
+    /// check all channels for zeroValue
+    /// </summary>
+    /// <returns>True if all channel equals zeroValue</returns>
+    protected virtual bool CheckAllChannelsZero()
+    {
+        for (int index = 0; index < BaseTelegram_ChannelBytesCount; index++)
+        {
+            if (_telegram_Base[BaseTelegram_ChannelStartOffset + index] != _zeroValueByte)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

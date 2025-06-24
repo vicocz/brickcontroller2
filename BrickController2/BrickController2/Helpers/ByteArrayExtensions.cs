8c5d17cda565acbac46bb9dfba8bf4efcb2a6762
J0EK3R <J0EK3R3.3@WIN11>
2025-06-24 05:06:49 +0200

    JK: revert reorder

--- BrickController2/BrickController2/Helpers/ByteArrayExtensions.cs
+++ BrickController2/BrickController2/Helpers/ByteArrayExtensions.cs
@@ -29,20 +29,6 @@ namespace BrickController2.Helpers
             return BitConverter.ToInt16(tempBuffer, 0);
         }
 
-        public static void SetUInt16(this byte[] data, ushort value, int offset = 0)
-        {
-            if (BitConverter.IsLittleEndian)
-            {
-                data[offset + 0] = (byte)(value & 0xff);
-                data[offset + 1] = (byte)((value >> 8) & 0xff);
-            }
-            else
-            {
-                data[offset + 1] = (byte)(value & 0xff);
-                data[offset + 0] = (byte)((value >> 8) & 0xff);
-            }
-        }
-
         public static ushort GetUInt16(this byte[] data, int offset = 0)
         {
             var tempBuffer = BitConverter.IsLittleEndian ?
@@ -57,6 +43,20 @@ namespace BrickController2.Helpers
                 new byte[] { data[offset + 0], data[offset + 1], data[offset + 2], data[offset + 3] } :
                 new byte[] { data[offset + 3], data[offset + 2], data[offset + 1], data[offset + 0] };
             return BitConverter.ToInt32(tempBuffer, 0);
+        }
+
+        public static void SetUInt16(this byte[] data, ushort value, int offset = 0)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                data[offset + 0] = (byte)(value & 0xff);
+                data[offset + 1] = (byte)((value >> 8) & 0xff);
+            }
+            else
+            {
+                data[offset + 1] = (byte)(value & 0xff);
+                data[offset + 0] = (byte)((value >> 8) & 0xff);
+            }
         }
 
         public static void SetInt32(this byte[] data, int value, int offset = 0)

8d9253e59be61f463c110bbcad2f89fdecbb15e7
Vit Nemecky <vico@centrum.cz>
2025-07-16 19:49:33 +0200

    use maui versinioning

--- BrickController2/BrickController2.iOS/PlatformServices/DI/PlatformServicesModule.cs
+++ BrickController2/BrickController2.iOS/PlatformServices/DI/PlatformServicesModule.cs
@@ -9,14 +9,12 @@ using BrickController2.iOS.PlatformServices.Infrared;
 using BrickController2.iOS.PlatformServices.Localization;
 using BrickController2.iOS.PlatformServices.Permission;
 using BrickController2.iOS.PlatformServices.SharedFileStorage;
-using BrickController2.iOS.PlatformServices.Versioning;
 using BrickController2.PlatformServices.BluetoothLE;
 using BrickController2.PlatformServices.GameController;
 using BrickController2.PlatformServices.Infrared;
 using BrickController2.PlatformServices.Localization;
 using BrickController2.PlatformServices.Permission;
 using BrickController2.PlatformServices.SharedFileStorage;
-using BrickController2.PlatformServices.Versioning;
 
 namespace BrickController2.iOS.PlatformServices.DI
 {
@@ -26,7 +24,6 @@ namespace BrickController2.iOS.PlatformServices.DI
         {
             builder.RegisterType<InfraredService>().As<IInfraredService>().SingleInstance();
             builder.RegisterType<GameControllerService>().AsSelf().As<IGameControllerService>().SingleInstance();
-            builder.RegisterType<VersionService>().As<IVersionService>().SingleInstance();
             builder.RegisterType<BluetoothLEService>().As<IBluetoothLEService>().SingleInstance();
             builder.RegisterType<LocalizationService>().As<ILocalizationService>().SingleInstance();
             builder.RegisterType<SharedFileStorageService>().As<ISharedFileStorageService>().SingleInstance();

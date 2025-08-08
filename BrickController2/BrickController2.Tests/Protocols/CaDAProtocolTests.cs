a736d48919fcb379d17352615ea5faa082f814f1
J0EK3R <J0EK3R3.3@WIN11>
2025-08-08 06:04:20 +0200

    JK: splitted unittest into two scenarios Encrypt/Decrypt

--- BrickController2/BrickController2.Tests/Protocols/CaDAProtocolTests.cs
+++ BrickController2/BrickController2.Tests/Protocols/CaDAProtocolTests.cs
@@ -17,9 +17,16 @@ public class CaDAProtocolTests
         CaDAProtocol.Encrypt(copy);
 
         copy.Should().BeEquivalentTo(expected);
+    }
 
+    [Theory]
+    [InlineData(new byte[] { 0x8a, 0xfb, 0x50, 0xfa, 0xdd, 0x73, 0x44, 0xcb, 0xde },
+                new byte[] { 0xaa, 0xbb, 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde })]
+    public void CaDAProtocol_Decrypt_ShouldReturnExpectedByteArray(byte[] input, byte[] expected)
+    {
+        byte[] copy = input.ToArray(); // Ensure we work with a copy
         CaDATools.Decrypt(copy);
 
-        copy.Should().BeEquivalentTo(input);
+        copy.Should().BeEquivalentTo(expected);
     }
 }

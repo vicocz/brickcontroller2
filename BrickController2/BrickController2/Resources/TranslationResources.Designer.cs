6e91a6896371a4513b3b4a7c3cfe176e70106766
J0EK3R <J0EK3R3.3@WIN11>
2025-10-03 13:23:42 +0200

    added translation resources "InputDevice" and "InputDevices"

--- BrickController2/BrickController2/Resources/TranslationResources.Designer.cs
+++ BrickController2/BrickController2/Resources/TranslationResources.Designer.cs
@@ -655,15 +655,6 @@ namespace BrickController2.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Controller tester.
-        /// </summary>
-        internal static string InputDeviceTester {
-            get {
-                return ResourceManager.GetString("InputDeviceTester", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Control point.
         /// </summary>
         internal static string ControlPoint {
@@ -1130,7 +1121,40 @@ namespace BrickController2.Resources {
                 return ResourceManager.GetString("Information", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to InputDevice.
+        /// </summary>
+        internal static string InputDevice
+        {
+            get
+            {
+                return ResourceManager.GetString("InputDevice", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to InputDevices.
+        /// </summary>
+        internal static string InputDevices
+        {
+            get
+            {
+                return ResourceManager.GetString("InputDevices", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to InputDevice tester.
+        /// </summary>
+        internal static string InputDeviceTester
+        {
+            get
+            {
+                return ResourceManager.GetString("InputDeviceTester", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to Interpolate.
         /// </summary>

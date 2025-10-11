8b8c9e1ac94fe0ca4ca8e86d4c6e26e74db60042
J0EK3R <J0EK3R3.3@WIN11>
2025-10-11 14:44:56 +0200

    adopted commit #c214753 ThumbStick gamepad buttons are not recognized [ios] (#178)
    https://github.com/vicocz/brickcontroller2/commit/c2147531439a6e55d1f5c18c1bfa23daa9c75032

--- BrickController2/BrickController2.iOS/PlatformServices/GameController/GamepadController.cs
+++ BrickController2/BrickController2.iOS/PlatformServices/GameController/GamepadController.cs
@@ -130,8 +130,21 @@ internal class GamepadController : InputDeviceBase<GCController>, IDisposable
 
         SetupDPadInput(gamePad.DPad, "DPad");
 
+        SetupDigitalOptionalButtonInput(gamePad.LeftThumbstickButton, "LeftThumbStick_Button");
+        SetupDigitalOptionalButtonInput(gamePad.RightThumbstickButton, "RightThumbStick_Button");
+
         SetupJoyInput(gamePad.LeftThumbstick, "LeftThumbStick");
         SetupJoyInput(gamePad.RightThumbstick, "RightThumbStick");
+    }
+
+    private void SetupDigitalOptionalButtonInput(GCControllerButtonInput? button, string name)
+    {
+        if (button is null)
+        {
+            return;
+        }
+
+        SetupDigitalButtonInput(button, name);
     }
 
     private void SetupDigitalButtonInput(GCControllerButtonInput button, string name)

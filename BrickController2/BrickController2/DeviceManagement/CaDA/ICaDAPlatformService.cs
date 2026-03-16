e82d539ec3053fb17afd198dc0bc4d2d9b524a5a
Vit Nemecky <vico@centrum.cz>
2026-03-16 18:59:21 +0100

    CADA RaceCar - support latest hw revision

--- BrickController2/BrickController2/DeviceManagement/CaDA/ICaDAPlatformService.cs
+++ BrickController2/BrickController2/DeviceManagement/CaDA/ICaDAPlatformService.cs
@@ -1,9 +1,19 @@
-﻿namespace BrickController2.DeviceManagement.CaDA;
+﻿using System;
+
+namespace BrickController2.DeviceManagement.CaDA;
 
 /// <summary>
 /// Interface definition for CaDA specific PlatformService
 /// </summary>
 public interface ICaDAPlatformService
 {
+    private const int V2PayloadLength = 16;
     bool TryGetRfPayload(byte[] rawData, out byte[] rfPayload);
+
+    bool TryGetRfPayloadV2(ReadOnlySpan<byte> rawData, out byte[] rfPayload)
+    {
+        // default implementation: copy raw data
+        rfPayload = rawData.ToArray();
+        return rawData.Length == V2PayloadLength;
+    }
 }

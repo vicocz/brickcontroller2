0cfc33817bdb6157e8c8e8a96a1590260b8b68ab
Vit Nemecky <vit.nemecky@centrum.cz>
2026-03-31 21:07:40 +0200

    MAUI .NET 10 [legacy] (#215)
    * MAUI .NET 10 (10.0.11 SR1.1) (#184)
    * Update dependencies: MAUI, Autofac, ZXing (#211)
    * fix PROPS / SLNX path
    * Resolve .NET 10 deprecation warnings (#187)

--- BrickController2/BrickController2.iOS/AppDelegate.cs
+++ BrickController2/BrickController2.iOS/AppDelegate.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
@@ -15,7 +14,6 @@ using BrickController2.iOS.UI.Services.DI;
 using BrickController2.UI.DI;
 using BrickController2.iOS.UI.CustomHandlers;
 using BrickController2.UI.Controls;
-using BrickController2.iOS.UI.CustomRenderers;
 using ZXing.Net.Maui.Controls;
 
 namespace BrickController2.iOS
@@ -33,7 +31,6 @@ namespace BrickController2.iOS
                 .ConfigureMauiHandlers(handlers =>
                 {
                     handlers.AddHandler<ExtendedSlider, ExtendedSliderHandler>();
-                    handlers.AddHandler(typeof(ListView), typeof(NoAnimListViewRenderer));
                 })
                 .UseBarcodeReader()
                 .ConfigureContainer(new AutofacServiceProviderFactory(), autofacBuilder =>

e65494cb1826a6184f50e60ddcab675b10bd0313
Vit Nemecky <vit.nemecky@centrum.cz>
2026-05-11 20:03:48 +0200

    LEGO Technic Move hub improvements [legacy] (#222)
    - add base class WirelessProtocolBasedDevice for lego based devices + move some logic from ControlPlusDevice
    - use OutputValuesGroup to better handle output values for Technic Move hub
    - use proper event waiting for servo reset / calibration for Technic Move hub

---
+++ BrickController2/BrickController2/DeviceManagement/Lego/ChannelPositionStore.cs
@@ -0,0 +1,12 @@
+﻿using BrickController2.DeviceManagement.IO;
+
+namespace BrickController2.DeviceManagement.Lego;
+
+internal class ChannelPositionStore : StateStore<int, PositionInfo>
+{
+    public ChannelPositionStore() : base(PositionInfo.Initial)
+    {
+    }
+
+    internal PositionInfo ConsumeUpdate(int channel) => Exchange(channel, x => x.ConsumeUpdate());
+}
